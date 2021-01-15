d115f00422ca5cf60f2e1639488d2202f659824b
Drewisatlas <drewisatlas@gmail.com>
2021-01-14 21:49:44 -0500

    feature: bootstrapped backend api and created contact controller

---
+++ asurity-project-backend/asurity-project-backend/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace AsurityProjectBackend.Models
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        //public string State { get; set; } //not sure how to best represent the dropdowns, maybe a state enum or class
+        public string Zipcode { get; set; }
+        //public string ContactFrequency { get; set; }
+        //public string ContactMethod { get; set; }
+    }
+}
---
+++ asurity-project-backend/asurity-project-backend/Repository/ContactContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AsurityProjectBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AsurityProjectBackend.Repository
+{
+    public class ContactContext: DbContext
+    {
+        public ContactContext(DbContextOptions<ContactContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
